e747ee2b3dad620da7f356327a3f175d147aadbf
Elijah Luckey <luckeyelijah112@gmail.com>
2019-03-19 13:46:52 -0400

    Added comments

--- in-class/trees.c
+++ in-class/trees.c
@@ -40,42 +40,39 @@
 #include <stdio.h>
 #include <time.h>
 
+// struct for the node object
 typedef struct node
 {
-    int data;
-    struct node *left, *right;
+    int data;                   // value stored in node
+    struct node *left, *right;  // pointers to next node
 }node;
 
-node *create_node(int data)
+node *create_node(int data) // returns a pointer with data passed in node 
 {
-    node *ptr = malloc(sizeof(node));
-    ptr->data = data;
-    ptr->left = ptr->right = NULL;
+    node *ptr = malloc(sizeof(node));   // allocates space
+    ptr->data = data;                   // sets value to data passed
+    ptr->left = ptr->right = NULL;      // only need to create this root node
 
     return ptr;
 }
 
 node *bst_insert(node *root, int data)
 {
-    if (root == NULL){
+    if (root == NULL)   // NULL check
         return create_node(data); // create_node returns a pointer to node
-    }
-
-    else if (data < root->data)
-    {
-        root->left = bst_insert(root->left, data); // recursive call to bst_insert() with new root node
-    } 
-    else if (data > root->data)
-    {
-        root->right = bst_insert(root->right, data);
-    } 
-    else
-    {
-        ;
-    }
     
+    else if (data < root->data)
+        // recursive call to bst_insert() with new root node
+        root->left = bst_insert(root->left, data);
+
+    else if (data > root->data)
+        root->right = bst_insert(root->right, data);
+
+    else
+        ;
 }
 
+// inorder algorithm traversal
 void inorder(node *root)
 {
     if (root = NULL)
@@ -86,6 +83,7 @@ void inorder(node *root)
     inorder(root->right);
 }
 
+// preorder algorithm traversal
 void preorder(node *root)
 {
     if (root = NULL)
@@ -96,6 +94,7 @@ void preorder(node *root)
     printf("%d ", root->data);
 }
 
+// postorder algorithm traversal
 void postorder(node *root)
 {
     if (root = NULL)
@@ -106,33 +105,35 @@ void postorder(node *root)
     postorder(root->right);
 }
 
-int max(int a, int b)
+int max(int a, int b) // aux func: input two values return greater values
 {
     return (a > b) ? a : b;
 }
 
+// return the height of the tree under the node
 int height(node *root)
 {
     if (root == NULL)
-        return -1;
+        return -1;  // base case
     
-    return 1 + max(height(root->left), height(root->right));
+    // returns 1 + each traversal
+    return 1 + max(height(root->left), height(root->right)); 
 }
 
+// set_fire() returns a pointer to a NULL node. It removes all roots/leaves following that node
 node *set_fire(node *root)
 {
-    if (root == NULL)
+    if (root == NULL)       // NULL condition
         return NULL;
 
-    set_fire(root->left);
-    set_fire(root->right);
-
-    free(root);
+    set_fire(root->left);   // traverse left
+    set_fire(root->right);  // traverse right
+    free(root);             // then remove
 
     return NULL;
 }
 
 int main()
 {
-    
+    // insert driver code here
 }
