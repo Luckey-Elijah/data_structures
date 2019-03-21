d6cd05637e65d731af4dc49717e8760709fc53fe
Elijah Luckey <luckeyelijah112@gmail.com>
2019-03-21 12:16:36 -0400

    added more function for BST

--- in-class/trees.c
+++ in-class/trees.c
@@ -34,7 +34,8 @@
 //     - move the child up in the tree
 
 // 3) The node has two children
-//     -  find max value of the left subtree. Then delete that value from the left subtree.
+//     -  find max value of the left subtree. Then delete that value 
+//        from the left subtree.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -59,7 +60,8 @@ node *create_node(int data) // returns a pointer with data passed in node
 node *bst_insert(node *root, int data)
 {
     if (root == NULL)   // NULL check
-        return create_node(data); // create_node returns a pointer to node
+        return create_node(data);   // create_node returns a 
+                                    // pointer to node
     
     else if (data < root->data)
         // recursive call to bst_insert() with new root node
@@ -105,7 +107,8 @@ void postorder(node *root)
     postorder(root->right);
 }
 
-int max(int a, int b) // aux func: input two values return greater values
+int max(int a, int b)   // aux func: input two values return greater 
+                        // values
 {
     return (a > b) ? a : b;
 }
@@ -120,7 +123,9 @@ int height(node *root)
     return 1 + max(height(root->left), height(root->right)); 
 }
 
-// set_fire() returns a pointer to a NULL node. It removes all roots/leaves following that node
+// set_fire() returns a pointer to a NULL node. It removes all roots/
+// leaves following that node
+
 node *set_fire(node *root)
 {
     if (root == NULL)       // NULL condition
@@ -133,7 +138,160 @@ node *set_fire(node *root)
     return NULL;
 }
 
+// When deleting a root node that has two children, replace the deleted
+// node with the greatest value on
+
+// function that deletes a node and re-orders a tree
+node *bst_delete(node *root, int data)
+{
+    node *new_root;
+
+    if (root->data > data)
+    {
+        root->left =bst_delete(root->left, data);
+    }
+    else if (root->data < data)
+    {
+        root->right = bst_delete(root->right, data);
+    }
+    else
+    {
+        // Hooray! We have found the value
+        // We first need to check the node for children
+        
+        // if no children, free the node
+        if (root->left == NULL && root->right == NULL)
+        {
+            free(root);
+            root = NULL;
+        }
+        else if (root->left == NULL)
+        {
+            // this case is when there is only a right child
+            new_root = root->right;
+            free(root);
+            return new_root;
+        }
+        else if (root->right == NULL)
+        {
+            new_root = root->left;
+            free(root);
+            return new_root;
+        }
+        else
+        {
+            // Case where the node to be deleted has two children
+            // We have to find the max node on the left subtree
+            root->data = find_max(root->left);
+            root->left = bst_delete(root->left, root->data);
+        }
+        
+    }
+    
+    return root;
+}
+
+// a function takes the root of a bst and returns a pointer
+// to the node with max value
+
+node *largest(node *root)
+{
+    // base case
+    if (root->right == NULL)
+    {
+        return root;
+    }
+
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    return largest(root->right);
+}
+
+// A function that takes the root of a bst and returns a pointer
+// to the node with max value
+node *iterative_largest(node *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    while ( root->right != NULL)
+    {
+        root = root->right;
+    }
+
+    return root;
+}
+
+// recursive approach for counting the number of nodes
+int count_nodes(node *root)
+{
+    return (root == NULL) ? 0 : count_nodes(root->left)
+     + count_nodes(root->right) +1;
+}
+
+// A function that counts the number of node that have one node
+int count_one_child_nodes(node *root)
+{
+    if (root = NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 0;
+    }
+    return (root->left == NULL || root->right == NULL)
+     + count_one_child_nodes(root->left)
+     + count_one_child_nodes(root->right);
+}
+
+// Return the number of leaf nodes in a binary tree
+int count_leaf_nodes(node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    if(root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return count_leaf_nodes(root->left)
+         + count_leaf_nodes(root->right);
+    }
+    
+}
+
+// return the value of the highest value node
+int find_max(node *root)
+{
+    while(root->right != NULL)
+    {
+        root = root->right;
+    }
+
+    return root->data;
+}
+
+int recursive_find_max(node *root)
+{
+    if (root->right == NULL)
+    {
+        return recursive_find_max(root->right);
+    }
+}
+
 int main()
 {
     // insert driver code here
+
+    // ask user what node they want to delete
 }
