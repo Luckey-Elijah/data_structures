d2377a57906dbf3198465f79a1c517cc860d8982
Elijah Luckey <luckeyelijah112@gmail.com>
2019-04-11 11:35:14 -0400

    more functionality of heap

---
+++ in-class/heap.c
@@ -0,0 +1,9 @@
+// adding node to binary heap:
+//     - O(logn)
+//     - "percolating"
+//     - since height of tree is O(logn)
+// deleting node from binary heap:
+//     -  move last node to root
+//     - percolate down
+//     - O(logn)
+
