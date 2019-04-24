301187203743fe594d3f046efbc3e1d9acdb51a9
Elijah Luckey <luckeyelijah112@gmail.com>
2019-04-24 14:44:57 -0400

    added final studies folder and documents

---
+++ final_studies/tries.c
@@ -0,0 +1,9 @@
+// Topics Covered:
+
+// 	- basic operations of tries:
+// 		- insertion
+// 		- retrieval
+// 		- deletion
+// 	- Big "O" runtimes
+// 	- deleting string from a tries
+// 		- delete nodes vs change values
