7557a95645ddc61626c13b7df63bcfda8f6f8553
Elijah Luckey <luckeyelijah112@gmail.com>
2019-04-24 16:19:45 -0400

    functions added, not completed though

--- final_studies/binary_trees.c
+++ final_studies/binary_trees.c
@@ -24,10 +24,47 @@
 // 		- pre-order
 // 		- post-order
 
+typedef struct node
+{
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+node *create_node(int new_data)
+{
+	node *new_node;
+	new_node = malloc(sizeof(node));
+
+	// checking here if we properlly allocated memory for node
+	if (new_node == NULL)
+	{
+		// print error message
+		printf("Could not allocate memory for new node.\nData: %d\n", new_data);
+
+		// return NULL pointer to node
+		return (new_node = NULL); 
+	}
+
+	new_node->data = new_data;
+	return new_node;
+}
+
+node *insert_node(node *root, int new_data)
+{
+	if (root == NULL)
+	{
+		printf("ROOT node is NULL, creating a new ROOT NODE.\n");
+		root = create_node(new_data);
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	// driver code for creating BSTs
-	int *node_values = [8, 11, 17, 14, 3, 5, 2, 21, 13, 12, 1];
+	int *node_values[] = {};
+
+	node *root = create_tree(node_values);
 
 	return 0;
 }
