00ed5862734d8f6edb0a2841ffc4d27a529f0cb2
Elijah Luckey <luckeyelijah112@gmail.com>
2019-04-26 18:31:12 -0400

    heap study started

--- final_studies/heaps.c
+++ final_studies/heaps.c
@@ -2,7 +2,7 @@
 
 // 	- structure and ordering of heap
 // 	- basic operations of heap
-// 		- know how-to tracfe through them
+// 		- know how-to trace through them
 // 	- when remove from heap, restore heapiness?
 // 	- insert into heap, new node goes where? heapiness restored?
 // 	- Big "O" runtimes
@@ -17,3 +17,26 @@
 
 // 	- know heapsort & Big "O" runtimes
 // 	- any other similar runtimes?
+
+// There two basic forms of binary heaps: MIN-heap and MAX-heap
+//      - MAX-heap: the key at the root must be greater than all of its children
+//      - MIN-heap: the key at the root must be less than than all of its children
+
+// Binary heaps can be classified by some properties:
+//      - it's a complete tree
+//      - it is either a min-heap or a max heap
+
+//  Binary heaps are typical represented as arrays
+
+// Common uses of HEAPS:
+
+    // - heapsort: uses a binary heap to sort an array in O(nlogn) time
+    // - priority queue
+    // - graph algorithms: such as Dijkstra's shortest path and Prim's minimum spanning tree
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *x, int *y);
+
+typedef struct
